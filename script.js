235e4c5e9ba9e5473663b6ea1cd195767b3240e7
Gratss <snaelzzz@gmail.com>
2023-02-07 19:34:05 +0300

    #8

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 "use strict";
-let num; // объявим переменную 
-num = 123; // присвоим ей значение
-alert(num); // выведем значение на экран 
+let a, b, c, d; 
 
-let a = 5 + 7;
-alert(a); // выведет 12
 
-let b = 7-5;
-alert(a); // выведет 2
+a = 10;
+b = 2;
+alert(a+b);
+alert(a-b);
+alert(a*b);
+alert(a/b);
 
-let c = 7 * 5;
-alert(c); // выведет 35
 
-let d = 7 / 5;
-alert(d); // выведет 1,4
+// Новая переменная "c" со значением 10 и переменная "d" со значением 5.
+c = 10;
+d = 5;
+result = c + d;
+alert(result);
 
-let e = 1 + 2 + 3;
-alert(e); // выведет 6
+// №3
+a = 1;
+b = 2;
+c = 3;
+result = a + b + c;
+alert(result);
+
+// №4 
+a = 10;
+b = 5;
+c = a - b;
+d = 7;
+result = c + d;
+alert(result);
