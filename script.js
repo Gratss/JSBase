de2b4b82952f43df669101d9ed6ede937d7064bb
Gratss <snaelzzz@gmail.com>
2023-02-07 19:28:31 +0300

    #6

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 "use strict";
-/*
-    комментарий
-*/
-alert('script1!'); // комментарий
+let num; 
+num = 123; 
+alert(num); 
+
+let a = 5 + 7;
+alert(a);
+
+let b = 7-5;
+alert(a); 
+
+let c = 7 * 5;
+alert(c); 
+
+let d = 7 / 5;
+alert(d); 
+
+let e = 1 + 2 + 3;
+alert(e); 
