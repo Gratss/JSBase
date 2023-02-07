84511689a2cd001391ca96f494f3a1e4ca0a1b77
Gratss <snaelzzz@gmail.com>
2023-02-07 19:29:01 +0300

    #7

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 "use strict";
-let num; 
-num = 123; 
-alert(num); 
+let num; // объявим переменную 
+num = 123; // присвоим ей значение
+alert(num); // выведем значение на экран 
 
 let a = 5 + 7;
-alert(a);
+alert(a); // выведет 12
 
 let b = 7-5;
-alert(a); 
+alert(a); // выведет 2
 
 let c = 7 * 5;
-alert(c); 
+alert(c); // выведет 35
 
 let d = 7 / 5;
-alert(d); 
+alert(d); // выведет 1,4
 
 let e = 1 + 2 + 3;
-alert(e); 
+alert(e); // выведет 6
