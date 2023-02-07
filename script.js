a2740ec523e14a6b5d8b50c8c92c9574b5d976b4
Gratss <snaelzzz@gmail.com>
2023-02-07 19:40:53 +0300

    #14

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
 "use strict";
-let a, b, c, d, result; 
+let a, b, c, d, result, str; 
 
+//Glebchikkk
 
-a = 2;
-alert(a ** 10);
- 
-//Glechikkk
-
-// #13
-a = 3 * 2 ** 3;
-alert(a);
-a = 3 * 2 ** 3;
-alert(a);
-a = 3 * 2 ** (3 + 1);
-alert(a);
-a = 2 ** 3 * 3;
-alert(a);
-a = 3 * 2 ** 3 * 3;
-alert(a);
+str = "Gleb"
+alert(str);
