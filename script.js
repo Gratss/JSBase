67819680408a05104caa58120412053012fafd56
Gratss <snaelzzz@gmail.com>
2023-02-07 19:42:14 +0300

    #15

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 "use strict";
-let a, b, c, d, result, str; 
+let a, b, c, d, result, str;
 
 //Glebchikkk
 
-str = "Gleb"
+str = "!!!"
 alert(str);
+a="biba";
+b="boba";
+alert(a + b);
+c="narik";
+d="slavik";
+alert(c + ' ' + d);
