2e784db280197dbca521588f237ddba271e2bfbf
Gratss <snaelzzz@gmail.com>
2023-02-07 19:51:16 +0300

    #25

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ let a, b, c, d, result, str;
 
 //Glebchikkk
 
-a = 5;
-b = 11;
-alert(String(a) + String(b));
-a = 137387341123213;
-alert(String(a).length);
-a = 2313132312132; // 13
-b = 31246786695; // 11
-alert(String(a).length + String(b).length); //24
+alert(true + 3);
+alert(true + true);
+alert(true - true);
+alert(true + false);
+alert('1' + true);
+alert( String(true) + 1 );
+alert( String(true) + Number(true) );
