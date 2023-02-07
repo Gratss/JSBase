b932042ec96780b4d10f6af1fe904e3c9da3d4ef
Gratss <snaelzzz@gmail.com>
2023-02-07 19:48:01 +0300

    #20

--- script.js
+++ script.js
@@ -3,15 +3,7 @@ let a, b, c, d, result, str;
 
 //Glebchikkk
 
-a = 'reju';
-console.log(a);
-b = 'za';
-c = 'hotdogi';
-d = 'zvoni';
-console.log(b, c, d);
-a = 123;
-b = '123';
-console.log(a, b);
-console.log(a);
-console.log(b);
-alert(reju);
+const PI = 3.14;
+let r = 10;
+result = 2 * PI * r;
+alert('Длина окружности с радиусом 10 равна ' + result);
