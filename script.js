6eb819a16a0545a5720b2af8858ef7b6858c14bb
Gratss <snaelzzz@gmail.com>
2023-02-07 19:49:25 +0300

    #23

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ let a, b, c, d, result, str;
 
 //Glebchikkk
 
-a = '10';
-b = '20';
-alert(Number(a) + Number(b));
-alert( Number('2') + Number('3') );
-alert( 2 + Number('3') );
-alert( '2' + Number('3') );
-a = '2';
-b = '3';
-alert(+a + +b);
+a = '5px';
+b = '6px';
+alert(parseInt(a) + parseInt(b));
+a = '5.5px';
+b = '6.25px';
+alert(parseFloat(a) + parseFloat(b));
+a = '5.5px';
+b = '6.25px';
+alert(parseFloat(a) + parseFloat(b) + 'px');
