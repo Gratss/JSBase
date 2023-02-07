6dc9652c6599a069b5857b779db0bba74c6dfa72
Gratss <snaelzzz@gmail.com>
2023-02-07 19:53:05 +0300

    #28

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ let a, b, c, d, result, str;
 //Glebchikkk
 
 a = 'abcde';
-alert(a[0]+ ' ' + a[2]+ ' ' +a[4]);
-b = a[4] + a[3] + a[2] + a[1] + a[0];
-alert(b);
-str = 'abcde';
-let num = 3;
-alert(str[num]);
+let last = a.length - 1;
+alert(a[last]);
+last = a.length - 2;
+alert(a[last]);
+last = a.length - 3;
+alert(a[last]);
