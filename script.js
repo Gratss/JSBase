94af90e1c5c0cc04f702654e0d637cdb9675b940
Gratss <snaelzzz@gmail.com>
2023-02-07 19:35:09 +0300

    #9

--- script.js
+++ script.js
@@ -2,31 +2,11 @@
 let a, b, c, d; 
 
 
-a = 10;
-b = 2;
-alert(a+b);
-alert(a-b);
-alert(a*b);
-alert(a/b);
-
-
-// Новая переменная "c" со значением 10 и переменная "d" со значением 5.
-c = 10;
-d = 5;
-result = c + d;
-alert(result);
-
+a = 2 * 8 / 4;
+alert(a);
+// №2
+a = (2 * 8) / 4;
+alert(a);
 // №3
-a = 1;
-b = 2;
-c = 3;
-result = a + b + c;
-alert(result);
-
-// №4 
-a = 10;
-b = 5;
-c = a - b;
-d = 7;
-result = c + d;
-alert(result);
+a = 2 * (8 / 4);
+alert(a);
