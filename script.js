1ad495a81400559c9c3e75675ac7ba1ede38e017
Gratss <snaelzzz@gmail.com>
2023-02-07 19:48:33 +0300

    #21

--- script.js
+++ script.js
@@ -3,7 +3,31 @@ let a, b, c, d, result, str;
 
 //Glebchikkk
 
-const PI = 3.14;
-let r = 10;
-result = 2 * PI * r;
-alert('Длина окружности с радиусом 10 равна ' + result);
+a = '5' + '2';
+alert(a);
+a = '5' + 2;
+alert(a);
+a = 5 + '2';
+alert(a);
+a = 5 + 2;
+alert(a);
+a = '5' * '2';
+alert(a);
+a = '5' - '2';
+alert(a);
+a = '5' / '2';
+alert(a);
+a = '5' % '2';
+alert(a);
+a = '5s' * '2';
+alert(a);
+a = '5s' + '2';
+alert(a);
+a = (-'5') + (-'2');
+alert(a);
+a = '5' * 1 + '2' * 1;
+alert(a);
+a = '5' * '1' + '2' * '1';
+alert(a);
+a = '' + 3 + 1;
+alert(a);
