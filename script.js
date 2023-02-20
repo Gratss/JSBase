158555af4fbf7d03c2cf74588a53987f31d90da2
Gratss <snaelzzz@gmail.com>
2023-02-20 18:16:16 +0300

    48

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-let arr = ['a', 'b', 'c', 'd', 'e'];
-delete arr[1];
-delete arr[3];
-console.log(arr.length); 
-console.log(arr);
+let week = {1: 'Понедельник', 
+2: 'Вторник', 
+3: 'Среда', 
+4: 'Четверг', 
+5: 'Пятница', 
+6: 'Суббота', 
+7: 'Воскресенье'};
+console.log(week);
