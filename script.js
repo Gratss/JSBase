59ca126a48f5bbfd9d797d4bd17c1daefbab5b57
Gratss <snaelzzz@gmail.com>
2023-02-20 18:07:47 +0300

    41

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-let arr1 = [1, 2, 3, 4, 5, 6];
-console.log(arr1[arr1.length - 1]) 
-let arr = [1, 2, 3, 4, 5, 6];
-console.log(arr.length);
+let arr1 = [1, 2, 3];
+arr1[0]++;
+arr1[1]++;
+arr1[2]++;
+console.log(arr1);
+let arr2 = [1, 2, 3, 4];
+arr2[0] += 3;
+arr2[1] += 3;
+arr2[2] += 3;
+arr2[3] += 3;
+console.log(arr2)
+let arr3 = ['a', 'b', 'c'];
+arr3[0] = 1;
+arr3[1] = 2;
+arr3[2] = 3;
