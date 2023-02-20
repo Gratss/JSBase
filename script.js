f5226694bb2e95251f361b44d1de74b3c497cb4a
Gratss <snaelzzz@gmail.com>
2023-02-20 17:58:45 +0300

    34

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-alert('Ваш возраст: ' + prompt('Ваш возраст?')); 
-// 2
-let num1 = prompt('Введите первое число');
-let num2 = prompt('Введите второе число');
-alert(Number(num1) + Number(num2)); 
-// 3
-let sguare = prompt('Сторона квадрата = ');
-alert(sguare*sguare);
-// 4
-let num3 = prompt('Введите первое число');
-let num4 = prompt('Введите второе число');
-alert(Number(num3) + Number(num4));
+document.write('GLEB');
+document.write('<p style="font-style: italic;">GLEB</p>');
+let str = 'text';
+document.write('<p style="font-style: italic;">SELIVANOV</p>');
+document.write('1<br>');
+document.write('2<br>');
+document.write('3<br>');
+document.write('4<br>');
+document.write('5<br>');
