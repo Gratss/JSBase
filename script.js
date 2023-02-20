5bf122bdebf013d6646a4bb89fcb448f1330f3a4
Gratss <snaelzzz@gmail.com>
2023-02-20 17:53:31 +0300

    31

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-let num = 1;
-num = num + 1;
-num = num + 1;
-console.log(num);
+let num = 10;
+num++;
+num++;
+num--;
+alert(num);
 // 2
-let num1 = 47;
-num1 += 7;
-num1 -= 18;
-num1 *= 10;
-num1 /= 15;
-console.log(num1);
+let num1 = 3;
+alert(++num1);
+// 3
+let num2 = 3;
+alert(num2++);
+// 4
+let num3 = 3;
+let num4 = ++num3;
+alert(num3);
+alert(num4);
+// 5
+let num5 = 3;
+let num6 = --num5;
+alert(num5);
+alert(num6)
