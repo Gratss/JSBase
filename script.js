df9e0e0597441b9de850da66dd7f3a22f2092af2
Gratss <snaelzzz@gmail.com>
2023-06-18 17:21:47 +0300

    308

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-let elem = document.getElementById('elem');
-
-function func(name, surname) {
-    console.log(this.value + ', ' + name 
-        + ' ' + surname); 
-}
-
-// привязываем контекст выполнения функции func к элементу elem
-let funcBound = func.bind(elem);
-
-funcBound('John', 'Smit'); // выводит 'привет, John Smit'
-funcBound('Eric', 'Luis');
+setInterval(function() {
+	console.log('Mavi');
+}, 3000);
