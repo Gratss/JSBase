f671d93be7575852098964bb8fec2c5759f248d1
Gratss <snaelzzz@gmail.com>
2023-06-18 17:36:09 +0300

    352

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 window.onload = function() {
+    function appendText(elem, text) {
+      elem.textContent += text;
+    }
+    
     let paragraphs = document.querySelectorAll('p');
     
-    paragraphs.forEach(function(paragraph, index) {
-      let paragraphNumber = index + 1;
-      paragraph.textContent = paragraphNumber + '. ' + paragraph.textContent;
+    paragraphs.forEach(function(paragraph) {
+      appendText(paragraph, '!');
     });
+    // 2
+    function setValue(input, text) {
+      input.value = text;
+    }
     
     }
