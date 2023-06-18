6a812242755faa079ec8969d899f001c9b220efe
Gratss <snaelzzz@gmail.com>
2023-06-18 17:23:20 +0300

    314

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 window.onload = function() {
-    let countdownInput = document.getElementById('elem');
-      let countdownValue = parseInt(countdownInput.value);
+    let elem = document.querySelector('#elem');
     
-      let timerId = setInterval(function() {
-        countdownValue--;
-        countdownInput.value = countdownValue;
-        if (countdownValue === 0) {
-          clearInterval(timerId);
-        }
+    elem.addEventListener('click', function() {
+      setInterval(function() {
+        elem.value = Number(elem.value) + 1;
       }, 1000);
+    });
     }
+    // 2
+    let elem = document.querySelector('#elem');
+    
+    elem.addEventListener('click', () => {
+      setInterval(() => {
+        elem.value = Number(elem.value) + 1;
+      }, 1000);
+    });
