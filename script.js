ec93f33ea1db5dcb898493551b11aaf7ebf651ae
Gratss <snaelzzz@gmail.com>
2023-06-18 17:22:35 +0300

    311

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-let number = 10;
-
-const timer = setInterval(() => {
-  console.log(number);
-  if (number === 0) {
-    clearInterval(timer);
-  } else {
-    number--;
+function startCountdown() {
+    let count = 100;
+    let countdown = setInterval(function() {
+      console.log(count);
+      count--;
+      if (count === 0) {
+        clearInterval(countdown);
+      }
+    }, 1000);
   }
+// 2
+let timerId;
+const countdownBtn = document.getElementById("countdownBtn");
+
+function startCountdown() {
+let counter = 100;
+timerId = setInterval(() => {
+console.log(counter);
+counter--;
+if (counter < 0) {
+  clearInterval(timerId);
+}
 }, 1000);
+}
+
+countdownBtn.addEventListener("click", () => {
+if (!timerId) {
+startCountdown();
+}
+});
