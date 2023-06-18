5804e04d8e568811bda70db043dbe7954afa76b7
Gratss <snaelzzz@gmail.com>
2023-06-18 17:30:31 +0300

    336

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
 window.onload = function() {
-    let parent = document.getElementById('parent');
-    let button = document.getElementById('button');
+    let elem   = document.querySelector('#elem');
+    let remove = document.querySelector('#remove');
     
-    button.addEventListener('click', function() {
-      let li = document.createElement('li');
-      li.innerHTML = parent.children.length + 1;
-      parent.appendChild(li);
-    
-      li.addEventListener('click', function() {
-        parent.removeChild(this);
-      });
+    remove.addEventListener('click', function(event) {
+        elem.remove();
+        event.preventDefault(); 
     });
-    
-    for (let i = 0; i < parent.children.length; i++) {
-      parent.children[i].addEventListener('click', function() {
-        parent.removeChild(this);
-      });
-    }
     }
