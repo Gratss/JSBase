ccb1233db7ca8fe5afbf5a7779a0325777a26e57
Gratss <snaelzzz@gmail.com>
2023-06-18 16:38:11 +0300

    196

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-let obj = {x: 1, y: 2, z: 3};
-console.log(obj.x);
-let obj1 = {x: 1, y: 2, z: 3};
-let key = 'x';
-console.log(obj1[key]);
-let obj2 = {x: 1, y: 2, z: 3};
-let sum = obj2.x + obj2.y + obj2.x;
-console.log(sum);
-let obj3 = {x: 1, y: 2, z: 3};
-console.log(Object.keys(obj3).length);
+let arr = [1, 2, 3, 4, 5];
+function func(arr) {
+	console.log(arr.shift()); 
+	console.log(arr); 
+
+	console.log(arr.shift()); 
+	console.log(arr); 
+
+	console.log(arr.shift()); 
+	console.log(arr); 
+}
+// 2
+let arr1 = [1, 2, 3, 4, 5];
+function getSum(arr1) {
+	let sum = arr1.shift();
+
+	if (arr1.length !== 0) {
+		sum += getSum(arr1);
+	}
+
+	return sum;
+}
+console.log(getSum());
